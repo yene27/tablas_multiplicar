c0ccf24aa11b99979f8ad2747c8e8e978c5b415f
yene27 <yene27@yahoo.com>
2022-06-16 07:45:15 -0500

    terminado

--- main.ts
+++ main.ts
@@ -1,15 +1,22 @@
 input.onButtonPressed(Button.A, function () {
     numero += 1
     basic.showNumber(numero)
-    soundExpression.hello.play()
 })
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     basic.showString("T:" + numero)
+    basic.pause(1000)
+    multiplicar(numero)
 })
 function multiplicar (núm: number) {
-    basic.showNumber(0 * numero)
+    contador = 1
+    for (let index = 0; index < 10; index++) {
+        basic.showString("" + contador + "=" + contador * numero)
+        basic.pause(500)
+        contador += 1
+    }
+    basic.clearScreen()
 }
+let contador = 0
 let numero = 0
 numero = 0
-basic.showNumber(numero)
