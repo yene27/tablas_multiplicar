920f0473ddff10cd635ab06b4f4f235365f9b20c
yene27 <yene27@yahoo.com>
2022-06-18 15:27:48 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.spring), SoundExpressionPlayMode.UntilDone)
+})
 input.onButtonPressed(Button.A, function () {
     numero += 1
     basic.showNumber(numero)
@@ -10,7 +13,8 @@ input.onButtonPressed(Button.B, function () {
 })
 function multiplicar (núm: number) {
     contador = 1
-    for (let index = 0; index < 10; index++) {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+    for (let index = 0; index < 5; index++) {
         basic.showString("" + contador + "=" + contador * numero)
         basic.pause(500)
         contador += 1
